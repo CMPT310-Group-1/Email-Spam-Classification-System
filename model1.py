391f1ea87ca9440fd7d7f0c422d75e258e2c4fd5
Danny Nguyen <giabaovip40011@gmail.com>
2026-06-26 22:16:29 -0700

    Add reqs.txt & update model.py

--- model1.py
+++ model1.py
@@ -3,9 +3,12 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import SGDClassifier
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.pipeline import make_pipeline
+
+# to save the model for stage 2
+import joblib
 
 data = pd.read_csv('combinned_data.csv')
 
@@ -33,16 +36,26 @@ vectorizer = CountVectorizer(
 )
 
 # binary classifier
-model = make_pipeline(vectorizer, 
-                      LogisticRegression(max_iter=1000))
+model = make_pipeline(vectorizer,
+                      SGDClassifier(        # SGD
+                        loss='log_loss',    # logistic loss function
+                        penalty='l2',       # regularization
+                        max_iter=1000,
+                        random_state=44
+                      )
+)
+                    
 model.fit(X_train, y_train)
 
 # prediction
 y_pred = model.predict(X_test)
 
-# Accuracy
+# Accuracy / Zero-one loss
 print("Accuracy:", accuracy_score(y_test, y_pred))
 
 # general report 
 print('report:')
 print(classification_report(y_test, y_pred))
+
+# save the model
+joblib.dump(model, 'stage1.pkl')
