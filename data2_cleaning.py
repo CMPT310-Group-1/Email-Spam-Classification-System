397c45d8528dba8356776737470c418b94141e77
jia <jisoo_im@sfu.ca>
2026-07-22 10:42:13 -0700

    data cleaning updated

--- data2_cleaning.py
+++ data2_cleaning.py
@@ -9,8 +9,4 @@ email_dataset = email_dataset[["text", "label", "category_name"]]
 # Rename columns
 email_dataset.columns = ["mod_text", "spam", "category"]
 
-# Check result
-print(email_dataset.head())
-
-# Save
 email_dataset.to_csv("cleaning_data.csv", index=False)
